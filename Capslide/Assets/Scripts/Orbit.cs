5a0e9bac0de2e1cc374d09d8e4be4cf4245a9baf
unknown <diamonick@aol.com>
2021-02-20 14:16:45 -0600

    Add 5 palettes

---
+++ Capslide/Assets/Scripts/Orbit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    private float angle;
+    [SerializeField] private Transform center;
+    [SerializeField] private float angleRate;
+    [SerializeField] private float radius;
+
+    private void OnEnable()
+    {
+        angle = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += angleRate;
+        this.gameObject.transform.position = RadialVector(center.position, radius);
+    }
+
+    private Vector3 RadialVector(Vector3 center, float radius)
+    {
+        Vector3 pos;
+        pos.x = center.x + radius * -Mathf.Sin(angle * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+}
